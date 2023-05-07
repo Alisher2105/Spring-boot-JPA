2aa749516c31e2fce4c229849c7f2563932a39aa
Alisher2105 <alisher.rajabov.2020@gmail.com>
2023-05-07 18:40:06 +0500

    Student umumiy ro`yhatni olish Studentni qo`shish Studentni id si bo`yicha olish Studentni o`chirish Studentni id bo`yicha o`zagrtirish

--- src/main/java/org/example/SpringbootJPA/controller/PersonController.java
+++ src/main/java/org/example/SpringbootJPA/controller/PersonController.java
@@ -1,9 +1,8 @@
 package org.example.SpringbootJPA.controller;
 
 import org.example.SpringbootJPA.model.Student;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.annotation.RequestScope;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -27,4 +26,61 @@ public class PersonController {
     public List <Student> getStudents(){
         return students;
     }
+
+    // bitta studentni olish
+    @RequestMapping(value = "/student/{id}", method = RequestMethod.GET)
+    public Student getStudentById(@PathVariable Integer id){
+        for (Student student : students) {
+            if(student.getId() == id){
+                return student;
+            }
+        }
+        return new Student();
+    }
+
+    //Client tarafdan json faylni gson orqali student clasiga o`tkazish
+    @RequestMapping(value = "/student",method = RequestMethod.POST)
+    public String addStudent(@RequestBody Student student){
+        student.setId(students.get(students.size() - 1).getId() + 1);
+        students.add(student);
+        return "Student qo`shildi";
+    }
+
+    // Delete
+    @RequestMapping(value = "/student/{id}", method =RequestMethod.DELETE)
+    public String deleteStudent(@PathVariable Integer id){
+        boolean deleted = false;
+        for (Student student : students) {
+            if(student.getId() == id){
+                students.remove(student);
+                deleted = true;
+                break;
+            }
+        }
+        return deleted?"Student o`chirildi":"Student topilmadi";
+    }
+
+
+    // EDITE(Studentni id si bo`yicha o`zgartirish)
+    @RequestMapping(value = "/student/{id}",method = RequestMethod.PUT)
+    public String editeStudent(@PathVariable Integer id,@RequestBody Student student){
+        boolean edited = false;
+        for (Student currentStudent : students) {
+            if (currentStudent.getId()== id){
+                currentStudent.setFirstName(student.getFirstName());
+                currentStudent.setLastName(student.getLastName());
+                currentStudent.setBirthDate(student.getBirthDate());
+                currentStudent.setPhoneNumber(student.getPhoneNumber());
+                edited = true;
+                break;
+            }
+        }
+        if (edited)  // if ning o`rniga return edited ? "Student o`zgartirildi" : "Student topilmadi";
+            return "Student o`zgartirildi";
+        else
+            return "Student Topilmadi";
+
+
+    }
+
 }
